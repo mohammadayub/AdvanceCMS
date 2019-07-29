1fe938c4288fdd3645b810d766ed7b0e0427d7d2
Mohammad Ayub <mohammadayub864@gmail.com>
2019-07-29 14:37:41 +0430

    Styling the Top Navigation

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,5 +7,17 @@ import Buefy from 'Buefy'
 
 Vue.use(Buefy);
 
+var app= new Vue({
+    el:'#app',
+    data:{}
+})
+
+$(document).ready(function(){
+    $('button.dropdown').hover(function(e){
+        $(this).toggleClass('is-open');
+
+    });
+});
+
 
 
